910f676bba1a5dc3343c22e45c6e35cf8fc2413d
Marc Lopez <marc5.12@outlook.com>
2015-08-06 10:23:02 +0200

    terraform consul backend

--- terraform.bash
+++ terraform.bash
@@ -2,9 +2,9 @@
 
 pre_run(){
 
-  docker-compose up --x-smart-recreate -d > /dev/null
-  docker-compose scale consulSlave=2
-  
+    docker-compose up --x-smart-recreate -d > /dev/null 2>&1
+    docker-compose scale consulSlave=2
+
 }
 
 terraform() {
@@ -21,5 +21,11 @@ terraform_cleanup() {
 
   docker-compose kill
   docker-compose rm -f
+
+}
+
+terraform_config() {
+
+  terraform remote config --backend consul -backend-config="address=consul:8500" -backend-config="path=tf"
 
 }
